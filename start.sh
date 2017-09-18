b4e13682708ef4ea0180c59cc9ba8165136ddb98
John T Skarbek <jtslear@gmail.com>
2017-09-18 08:11:00 -0400

    loggly ignores the name of the tag, it only accepts the value

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ config="/etc/rsyslog.conf"
 echo "Creating config file..."
 
 cat << EOF >> ${config}
-template(name="LogglyFormat" type="string" string="<%pri%>%protocol-version% %timestamp:::date-rfc3339% ${HOSTNAME} %app-name% %procid% %msgid% [${LOGGLY_TOKEN}@41058 tag=\"haproxy\" environment=\"${ENVIRONMENT}\"] %msg%\n")
+template(name="LogglyFormat" type="string" string="<%pri%>%protocol-version% %timestamp:::date-rfc3339% ${HOSTNAME} %app-name% %procid% %msgid% [${LOGGLY_TOKEN}@41058 tag=\"haproxy\" tag=\"${ENVIRONMENT}\"] %msg%\n")
 
 action(type="omfwd" protocol="tcp" target="logs-01.loggly.com" port="514" template="LogglyFormat")
 
